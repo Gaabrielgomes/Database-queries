8c558ba2c5defba8b26d545f7aa12eff20ac5f6b
GaabrielGomes <gaabrielgomes3241@gmail.com>
2026-02-09 15:27:05 -0300

    Added code to connect to the database

---
+++ src/connection.py
@@ -0,0 +1,27 @@
+import os
+import pyodbc
+from dotenv import load_dotenv
+
+
+def connect_database():
+    
+    load_dotenv()
+
+    conn_str = (
+        f"DRIVER={os.getenv('DRIVER')};"
+        f"UID={os.getenv('UID')};"
+        f"PWD={os.getenv('PWD')};"
+        f"ENG={os.getenv('ENG')};"
+        f"DBN={os.getenv('DBN')};"
+        f"LINKS={os.getenv('LINKS')};"       
+    )
+
+    try:
+        conn = pyodbc.connect(conn_str)
+        print("\rConnecting to database... Done!", flush=True)
+        return conn
+    
+    except Exception as error:
+        print(f"\rConnecting to database... Failed!", flush=True)
+        print(f"Error connecting to the database: {str(error)}")
+        return None
